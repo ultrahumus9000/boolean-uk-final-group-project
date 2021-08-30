06616fe121702fb4a29e88480c4b3da5aa6d1e71
Linlin Li <paris1900lll@gmail.com>
2021-08-30 16:29:31 +0100

    set up

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,9 +3,6 @@ var express = require("express");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
 var app = express();
 
 app.use(logger("dev"));
