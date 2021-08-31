d5878a65a61a908bcd312f6947a916315fa412dd
Linlin Li <paris1900lll@gmail.com>
2021-08-31 17:05:02 +0100

    add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
 
+// import flatpickr from "flatpickr";
+{
+  /* <link rel="stylesheet" type="text/css" href="https://npmcdn.com/flatpickr/dist/themes/material_green.css"> */
+}
+
 function App() {
   return <div className="App"></div>;
 }
