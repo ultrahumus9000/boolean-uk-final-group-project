60759408096618eeacb4a4bec4b918f5c564f4cf
Linlin Li <paris1900lll@gmail.com>
2021-09-01 09:49:33 +0100

    add token

---
+++ backend/src/authgenerator.ts
@@ -0,0 +1,16 @@
+import Jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT as string;
+
+function createToken(payload: Jwt.JwtPayload) {
+  return Jwt.sign(payload, JWT_SECRET);
+}
+
+function validateToken(token: string) {
+  return Jwt.verify(token, JWT_SECRET);
+}
+
+export { createToken, validateToken };
