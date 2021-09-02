3c98ebf9324ee6de6815ed17da24e8eb432ce1af
Linlin Li <paris1900lll@gmail.com>
2021-09-02 17:11:12 +0100

    x

--- backend/dist/guest/controller.js
+++ backend/dist/guest/controller.js
@@ -13,7 +13,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = __importDefault(require("../database"));
-const { user, guestProfile } = database_1.default;
+const { user } = database_1.default;
 function getGuestProfile(req, res) {
     var _a;
     return __awaiter(this, void 0, void 0, function* () {
