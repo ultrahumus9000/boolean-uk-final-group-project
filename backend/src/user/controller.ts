0161f2f6d6b8149bc7dcb907a18aa894ce5c6363
Linlin Li <paris1900lll@gmail.com>
2021-09-02 16:21:26 +0100

    changes

--- backend/src/user/controller.ts
+++ backend/src/user/controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import db from "../database";
-import { createToken } from "../authgenerator";
+
 import createNewUserWithHash from "./service";
 
 const { hostProfile, guestProfile } = db;
