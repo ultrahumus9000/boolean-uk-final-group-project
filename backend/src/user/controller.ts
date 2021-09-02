ee73f7a04d1ef3029379acb33c1e9df0d625cd12
Linlin Li <paris1900lll@gmail.com>
2021-09-02 16:21:00 +0100

    signupworks

--- backend/src/user/controller.ts
+++ backend/src/user/controller.ts
@@ -7,25 +7,29 @@ const { hostProfile, guestProfile } = db;
 
 async function createNewUser(req: Request, res: Response) {
   const newUser = req.body;
+  try {
+    const modifiedUser = await createNewUserWithHash(newUser);
 
-  const modifiedUser = await createNewUserWithHash(newUser);
-
-  if (newUser.guestProfile) {
-    const result = await guestProfile.create({
-      data: {
-        bio: newUser.bio,
-        userId: modifiedUser.id,
-      },
-    });
-    res.json(result);
-  } else {
-    const result = await hostProfile.create({
-      data: {
-        bio: newUser.bio,
-        userId: modifiedUser.id,
-      },
-    });
-    res.json(result);
+    if (newUser.guestProfile) {
+      const result = await guestProfile.create({
+        data: {
+          bio: newUser.bio,
+          userId: modifiedUser.id,
+        },
+      });
+      res.json(result);
+    } else {
+      const result = await hostProfile.create({
+        data: {
+          bio: newUser.bio,
+          userId: modifiedUser.id,
+        },
+      });
+      res.json(result);
+    }
+  } catch (error) {
+    console.log(error);
+    res.json(error);
   }
 }
 
