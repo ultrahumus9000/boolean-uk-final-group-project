375f29184c392c33eb0c8a2bcc846e90d89b8ded
Linlin Li <paris1900lll@gmail.com>
2021-09-03 18:48:33 +0100

    save

--- backend/dist/middleware.js
+++ backend/dist/middleware.js
@@ -4,10 +4,6 @@ const authgenerator_1 = require("./authgenerator");
 exports.default = (req, res, next) => {
     const { token } = req.cookies;
     let userData = token && (0, authgenerator_1.validateToken)(token);
-    userData = {
-        id: 2,
-        username: "Ceasar.Pollichbcom",
-    };
     if (userData) {
         req.currentUser = userData;
         next();
