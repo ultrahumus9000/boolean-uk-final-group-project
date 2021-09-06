63271b4f2ec35e3f2ddb1469c5c325a3e5d9ca0a
Lizzie Hand <elizabethghand@gmail.com>
2021-09-06 14:44:37 +0100

    guest dash

--- frontend/src/components/BottomNav.tsx
+++ frontend/src/components/BottomNav.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@material-ui/core"
 import React from "react"
+import { Link } from "react-router-dom"
 import heart from "../assets/heart.png"
 import loupe from "../assets/loupe.png"
 
@@ -8,7 +9,9 @@ export default function BottomNav() {
     return (
         <div className="bottom-nav">
             <img src={heart}></img>
-            <img src={loupe}></img>
+            <Link to="/guest/dashboard">
+                <img src={loupe}></img>
+            </Link>
             <Button variant="contained" color="secondary"> Switch to host</Button>
         </div>
     )
--- frontend/src/components/PastBookings.tsx
+++ frontend/src/components/PastBookings.tsx
@@ -1,19 +1,27 @@
 import React from "react"
 import { Button } from "@material-ui/core"
 
-export default function PastBookings() {
+export default function PastBookings({ userBookings }) {
+
+   console.log("userBookings", userBookings)
    return (
-      <div className="stay-container">
-         <img src="#"></img>
-         <div className="stay-details">
-            <div className="hotelName">
-               <p className="stay-title">Past Name</p>
-               <p> 1-3st Sep</p>
-            </div>
-            <div className="contact-host">
-               <Button> Leave review</Button>
-            </div>
-         </div>
-      </div>
+      <>
+         {
+            userBookings.map((booking) =>
+               <div className="stay-container">
+                  <img src="#"></img>
+                  <div className="stay-details">
+                     <div className="hotelName">
+                        <p className="stay-title">{booking.name}</p>
+                        <p> 1-3st Sep</p>
+                     </div>
+                     <div className="contact-host">
+                        <Button> Leave review</Button>
+                     </div>
+                  </div>
+               </div>
+            )
+         }
+      </>
    )
 }
