8939d3f2033e6a4d91da25fbe9d9f3a7416aaf87
Camilla Vidot <camillavidot@gmail.com>
2021-09-09 13:14:51 +0100

    updated env file

--- backend/src/authgenerator.ts
+++ backend/src/authgenerator.ts
@@ -1,17 +1,17 @@
-import Jwt from "jsonwebtoken";
-import dotenv from "dotenv";
+import Jwt from "jsonwebtoken"
+import dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config()
 
-const JWT_SECRET = process.env.JWT as string;
+const JWT_SECRET = process.env.JWT as string
 
 function createToken(payload: Jwt.JwtPayload) {
-  console.log("create token", Jwt.sign(payload, JWT_SECRET));
-  return Jwt.sign(payload, JWT_SECRET);
+  console.log("create token", Jwt.sign(payload, JWT_SECRET))
+  return Jwt.sign(payload, JWT_SECRET)
 }
 
 function validateToken(token: string) {
-  return Jwt.verify(token, JWT_SECRET);
+  return Jwt.verify(token, JWT_SECRET)
 }
 
-export { createToken, validateToken };
+export { createToken, validateToken }
